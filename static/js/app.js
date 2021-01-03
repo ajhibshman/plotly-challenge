c744ca08d21a74ccfe1781c4df349be41ee8f53b
ajhibshman <ajhibshman@hotmail.com>
2021-01-03 16:13:33 -0700

    complete basic scripts

--- static/js/app.js
+++ static/js/app.js
@@ -6,9 +6,6 @@ d3.json("static/samples.json").then((importedData) => {
 
     //create dropdown list
     // select list of names
-    
-    //var names = data.map(x => x.names);
-    //console.log(names);
     var ids = data[0].names;
     console.log(ids);
 
@@ -18,87 +15,225 @@ d3.json("static/samples.json").then((importedData) => {
         var item = id_select.append("option");
         item.text(id);
     });
-    // get selection as var
-    var inputElement = d3.select("#selDataset");
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
 
-    //retrieve sample data for plotting
-    var samples = data[0].samples;
-    console.log(samples);
+    function init(){
+        // get selection as var
+        var inputElement = d3.select("#selDataset");
+        var inputValue = inputElement.property("value");
+        console.log(inputValue);
 
-    //filter for selected input
-    function filterdata(sample) {
-        return sample.id === inputValue;
-    }
+        //retrieve sample data for plotting
+        var samples = data[0].samples;
+        console.log(samples);
 
-    var initdata = samples.filter(filterdata);
-    console.log(initdata);
-
-    //create horizontal bar chart (bar)
-    //assign variables to samples data
-    var otu_ids = initdata[0].otu_ids;
-    otu_ids=otu_ids.map(String);
-    var sample_values = initdata[0].sample_values;
-    var otu_labels = initdata[0].otu_labels;
-
-    console.log(otu_ids);
-    
-     //slice top 10
-    var top_ids = otu_ids.slice(0,10);
-
-    var top_values = sample_values.slice(0,10);
-    var top_labels = otu_labels.slice(0,10);
-
-    console.log(top_ids);
-    console.log(top_values);
-    console.log(top_labels);
-
-    var trace1 = {
-        type:'bar',
-        x: top_values,
-        y: top_ids,
-        orientation: 'h'
-    }
-
-    var plot_data = [trace1];
-
-    var layout = {
-        yaxis: {
-            type:'category'
+        //filter for selected input
+        function filterdata(sample) {
+            return sample.id === inputValue;
         }
 
+        var initdata = samples.filter(filterdata);
+        console.log(initdata);
+
+        //create horizontal bar chart (bar)
+        //assign variables to samples data
+        var otu_ids = initdata[0].otu_ids;
+        otu_ids=otu_ids.map(String);
+        var sample_values = initdata[0].sample_values;
+        var otu_labels = initdata[0].otu_labels;
+
+        console.log(otu_ids);
+    
+         //slice top 10
+        var top_ids = otu_ids.slice(0,10).reverse();
+
+        var top_values = sample_values.slice(0,10).reverse();
+        var top_labels = otu_labels.slice(0,10).reverse();
+
+        console.log(top_ids);
+        console.log(top_values);
+        console.log(top_labels);
+
+        var trace1 = {
+            type:'bar',
+            x: top_values,
+            y: top_ids,
+            orientation: 'h',
+            text: otu_labels
+        }
+
+        var plot_data = [trace1];
+
+        var layout = {
+            title: 'Top 10 OTUs in selected subject ID<br> Hover for bacteria label',
+            yaxis: {
+                type:'category',
+                title: 'OTU Id #'
+            },
+            xaxis: {
+            title:'Frequency found'
+            }
+        }
+
+        Plotly.newPlot("bar",plot_data, layout);
+  
+        //create bubble chart
+        // otu_ids as x values and marker color
+        // samples as y values and marker size
+        // otu_labels as text values 
+
+        var trace2 = {
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            text : otu_labels,
+            marker: {
+                size: sample_values,
+                color: otu_ids
+            }
+        };
+
+        var data2 = [trace2];
+
+        var layout2 = {
+            title: 'Samples Frequency per otu_id for Subject ID,<br>Size indicates Frequency<br>Hover for Bacteria Label',
+            showlegend: false,
+            xaxis: {
+                title:"OTU ID #"
+            },
+            yaxis:{
+             title:"Frequency found"
+            }
+        };
+
+        Plotly.newPlot('bubble',data2,layout2);
+
+        //display metadata
+        var metas = data[0].metadata;
+        console.log(metas);
+
+        //filter for selected input
+        function filterdata2(subject) {
+            return subject.id == inputValue;
+        }
+
+        var initmeta = metas.filter(filterdata2);
+        console.log(initmeta);
+
+        var meta_select=d3.select("#sample-metadata")
+        initmeta.forEach((combo)=> {
+            Object.entries(combo).forEach(([key, value])=> {
+                var item = meta_select.append("p");
+                item.text(`${key}: ${value}`);
+            });
+        });
     }
-
-    Plotly.newPlot("bar",plot_data, layout);
-
-
-   
-
-    //create bubble chart
-    // otu_ids as x values and marker color
-    // samples as y values and marker size
-    // otu_labels as text values 
-
-
-
-    //display metadata
-
 
 
     //select drop down and execute update code
-    //d3.selectAll("#selDataset").on("change", updatePlotly);
+    d3.selectAll("#selDataset").on("change", updatePlotly);
+    function updatePlotly(){
+        // get selection as var
+        var inputElement = d3.select("#selDataset");
+        var inputValue = inputElement.property("value");
+        
+        //retrieve sample data for plotting
+        var samples = data[0].samples;
+        
+        //filter for selected input
+        function filterdata(sample) {
+            return sample.id === inputValue;
+        }
 
-    // This function is called when a dropdown menu item is selected
-    //function updatePlotly() {
-        // Use D3 to select the dropdown menu
-        //var dropdownMenu = d3.select("#selDataset");
-        // Assign the value of the dropdown menu option to a variable
-        //var dataset = dropdownMenu.property("value");
+        var initdata = samples.filter(filterdata);
+        
+        //create horizontal bar chart (bar)
+        //assign variables to samples data
+        var otu_ids = initdata[0].otu_ids;
+        otu_ids=otu_ids.map(String);
+        var sample_values = initdata[0].sample_values;
+        var otu_labels = initdata[0].otu_labels;
 
+        //slice top 10
+        var top_ids = otu_ids.slice(0,10).reverse();
 
+        var top_values = sample_values.slice(0,10).reverse();
+        var top_labels = otu_labels.slice(0,10).reverse();
 
+        var trace1 = {
+            type:'bar',
+            x: top_values,
+            y: top_ids,
+            orientation: 'h',
+            text: otu_labels
+        }
 
+        var plot_data = [trace1];
 
+        var layout = {
+            title: 'Top 10 OTUs in selected subject ID<br> Hover for bacteria label',
+            yaxis: {
+               type:'category',
+               title: 'OTU Id #'
+            },
+            xaxis: {
+               title:'Frequency found'
+            }
+        }
 
+        Plotly.newPlot("bar",plot_data, layout);
+  
+        //create bubble chart
+        // otu_ids as x values and marker color
+        // samples as y values and marker size
+        // otu_labels as text values 
+
+        var trace2 = {
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            text : otu_labels,
+            marker: {
+                size: sample_values,
+                color: otu_ids
+            }
+        };
+
+        var data2 = [trace2];
+
+        var layout2 = {
+            title: 'Samples Frequency per otu_id for Subject ID,<br>Size indicates Frequency<br>Hover for Bacteria Label',
+            showlegend: false,
+            xaxis: {
+             title:"OTU ID #"
+            },
+            yaxis:{
+            title:"Frequency found"
+            }
+
+        };
+
+        Plotly.newPlot('bubble',data2,layout2);
+
+        //display metadata
+        var metas = data[0].metadata;
+        
+        //filter for selected input
+        function filterdata2(subject) {
+            return subject.id == inputValue;
+        }
+
+        var initmeta = metas.filter(filterdata2);
+        
+        var meta_select=d3.select("#sample-metadata")
+        meta_select.html("");
+        initmeta.forEach((combo)=> {
+            Object.entries(combo).forEach(([key, value])=> {
+                var item = meta_select.append("p");
+                item.text(`${key}: ${value}`);
+            });
+        })
+    };
+
+    init();
+  
 });
---
+++ static/js/bonus.js
@@ -0,0 +1,25 @@
+//gauge chart 
+//"gauge" id in index
+
+d3.json("static/samples.json").then((importedData2) => {
+    var data2 = importedData2;
+    console.log(data2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
